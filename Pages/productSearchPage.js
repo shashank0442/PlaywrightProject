21f95fe9b6cb9c761656a76458b9f52cd668d794
shashank0442 <shashank.0442@gmail.com>
2024-11-11 07:09:20 +0530

    Added more steps in search page

--- Pages/productSearchPage.js
+++ Pages/productSearchPage.js
@@ -5,13 +5,59 @@ exports.ProductSearchPage = class ProductSearchPage {
     /**
      * @param {import('@playwright/test').Page} page
      */
-    constructor(page) { 
+    constructor(page) {
         this.page = page;
         this.giftCardMenuButton = page.getByRole('link', { name: 'Gift Cards' }).first();
         this.addToCartButton = page.getByRole('button', { name: 'Add to cart' }).first();
-        
-
         this.productQuantity = page.getByLabel('Qty:');
-        this.addToCartButtonOnProductDetailPage = page.locator('#add-to-cart-button-1')
+
+        this.giftCardHeader = page.locator('h1');
+        this.giftCardName = page.getByRole('link', { name: '$5 Virtual Gift Card', exact: true });
+        this.redeemMessage = page.locator('#product-details-form');
+
+        this.recipientNameLocator = page.getByLabel('Recipient\'s Name:');
+        this.recipientEmailLocator = page.getByLabel('Recipient\'s Email:');
+        this.singleProductCostLocator = page.locator('#product-details-form');
+        this.addToCartButtonLocator = page.locator('#topcartlink a span.cart-label');
+        this.itemQuantityLocator = page.locator('input[name="itemquantity4710008"]');
+        this.updateShoppingCart = page.getByRole('button', { name: 'Update shopping cart' })
+        this.termsLocator = page.locator('#termsofservice');
+
+        this.addToCartButtonOnProductDetailPage = page.locator('#add-to-cart-button-1');
+        this.continueButtonOnBillingAddress = page.getByRole('button', { name: 'Continue' });
+
+        this.checkoutButton = page.getByRole('button', { name: 'Checkout' });
+
     }
+
+    async validateBillingAddressErrorMessage() {
+        let errorMessage = ["Country is required.", "City is required", "Street address is required", "Zip / postal code is required", "Phone is required"];
+        for (let i = 0; i < errorMessage.length; i++) {
+            await expect(this.page.locator('#billing-new-address-form')).toContainText(errorMessage[i]);
+        }
+    }
+
+    async fillFormValues() {
+        let formLabels = ["Company:", "City:", "Address 1:", "Address 2:", "Zip / postal code:", "Phone number:", "Fax number:"];
+        let formValues = ["ABC Private LTD", "Pune", "abc line", "Xyz Colony", "4433322", "9876543210", "9876987699"];
+
+        for (let i = 0; i < formLabels.length; i++) {
+            for (let j = i; j <= i; j++) {
+                await this.page.getByLabel(formLabels[i]).fill(formValues[j]);
+                console.log(formLabels[i] + "  " + formValues[j])
+            }
+        }
+    }
+
+    async removeExistingAddress() {
+        await this.page.goto("https://demowebshop.tricentis.com/customer/addresses");
+        await this.page.getByRole('button', { name: 'Delete' }).click();
+            this.page.once('dialog', dialog => {
+                console.log(`Dialog message: ${dialog.message()}`);
+                dialog.accept();
+            });
+        this.page.on('dialog', dialog => dialog.accept());
+        await page.getByRole('OK').click();
+    }
+
 }
--- tests/ProductSearch.spec.js
+++ tests/ProductSearch.spec.js
@@ -1,31 +1,107 @@
 import { test, expect } from '@playwright/test';
 import { ProductSearchPage } from "../Pages/productSearchPage";
 import { LoginPage } from '../Pages/loginPage';
+import { log } from 'console';
 
 test('Verify login functionality', async ({ page }) => {
+    let username = 'test.pass1@gmail.com';
+    let password = 'TestPass@123';
+    let firstName = 'test';
+    let lastName = 'pass1';
+    let redeemText = '$5 Gift Card. Gift Cards must be redeemed through our site Web site toward the purchase of eligible products.';
+    let recipientName = 'Ron';
+    let recipientEmail = 'ron@gmail.com';
+
     const loginPage = new LoginPage(page);
     const productSearchPage = new ProductSearchPage(page);
     await loginPage.openLoginPage();
-    await loginPage.login('test.pass1@gmail.com','TestPass@123');
+    await loginPage.login(username, password);
 
-    await page.pause();
+
+    //-------------------------Need to run again ------------------------------
+
+    if (!await page.getByText('No addresses').isVisible()) {
+        await page.goto("https://demowebshop.tricentis.com/customer/addresses");
+        while (!await page.getByText('No addresses').isVisible()) {
+            await page.getByRole('button', { name: 'Delete' }).click();
+            page.on('dialog', dialog => dialog.accept());
+        }
+    }
+    //-------------------------------------------------------------------------
+
+    //await productSearchPage.removeExistingAddress();
+
+
+
     await productSearchPage.giftCardMenuButton.click();
-    await expect(page.locator('h1')).toContainText('Gift Cards');
-    await expect(page.locator('body')).toContainText('$5 Virtual Gift Card');
-    await expect(page.locator('body')).toContainText('5.00');
-    await page.getByRole('button', { name: 'Add to cart' }).first().click();
-    await expect(page.locator('body')).toContainText('$5 Virtual Gift Card');
-    await expect(page.locator('#product-details-form')).toContainText('$5 Gift Card. Gift Cards must be redeemed through our site Web site toward the purchase of eligible products.');
-    
-    await page.getByLabel('Recipient\'s Name:').fill('Ram');
-    await page.getByLabel('Recipient\'s Email:').fill('Ram@gmail.com');
-    await expect(page.locator('#product-details-form')).toContainText('5.00');
 
+    // Home / Gift Cards Screen - Checking details on GiftCard Screen
+    await expect(productSearchPage.giftCardHeader).toContainText('Gift Cards');
+    await expect(productSearchPage.giftCardName).toBeVisible();
+    await productSearchPage.addToCartButton.click();
+
+    //Home / Gift Cards / $5 Virtual Gift Card
+    await expect(productSearchPage.redeemMessage).toContainText(redeemText);
+    await productSearchPage.recipientNameLocator.fill(recipientName);
+    await productSearchPage.recipientEmailLocator.fill(recipientEmail);
+
+    await page.getByRole('input', { value: firstName + " " + lastName }).isVisible();
+    await page.getByRole('input', { value: username }).isVisible();
+    await expect(productSearchPage.singleProductCostLocator).toContainText('5.00');
     await productSearchPage.productQuantity.fill('2');
     await productSearchPage.addToCartButtonOnProductDetailPage.click();
-    await page.getByRole('link', { name: 'Shopping cart (2)' }).click();
-    await expect(page.locator('body')).toContainText('10.00');
-    await page.locator('#termsofservice').check();
-    await expect(page.locator('body')).toContainText('I agree with the terms of service and I adhere to them unconditionally (read)');
-    await page.getByRole('button', { name: 'Checkout' }).click();
+
+    //add to cart
+    await productSearchPage.addToCartButtonLocator.click();
+    let itemQuantity = await productSearchPage.itemQuantityLocator.getAttribute('value');
+    console.log("itemQuantity == " + itemQuantity);
+
+    if (!itemQuantity == 2) {
+        await productSearchPage.itemQuantityLocator.fill('2');
+        await productSearchPage.updateShoppingCart.click();
+    }
+
+    await productSearchPage.termsLocator.check();
+    await productSearchPage.checkoutButton.click();
+
+    let prepopulatedValues = [firstName, lastName, username];
+    for (let i = 0; i < prepopulatedValues.length; i++) {
+        await page.getByRole('input', { value: prepopulatedValues[i] }).isVisible();
+    }
+
+    //Validating Error message on the page.
+    await productSearchPage.continueButtonOnBillingAddress.click();
+    await productSearchPage.validateBillingAddressErrorMessage();
+
+    await page.getByLabel('Country:').selectOption('41');
+
+    await productSearchPage.fillFormValues();
+
+    await productSearchPage.continueButtonOnBillingAddress.click();
+    await expect(page.locator('#opc-payment_method')).toContainText('Payment method');
+
+    await expect(page.locator('#checkout-payment-method-load')).toContainText('Cash On Delivery (COD) (7.00)');
+    await page.getByRole('button', { name: 'Continue' }).click();
+
+    await page.pause();
+    await page.locator('#payment-info-buttons-container').click();
+    await expect(page.locator('#opc-payment_info')).toContainText('Payment information');
+    await expect(page.getByRole('cell')).toContainText('You will pay by COD');
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await page.locator('#checkout-confirm-order-load li').filter({ hasText: 'Billing Address' }).click();
+    await expect(page.locator('#checkout-confirm-order-load')).toContainText('Billing Address');
+   // await expect(page.locator('#checkout-confirm-order-load')).toContainText('Billing Address test pass Email: test.pass1@gmail.com Phone: 9876543210 Fax: 9876987699 ABC Private LTD a b Pune , 111111 India Payment Method Cash On Delivery (COD)');
+    await expect(page.getByText('10.00').nth(1)).toBeVisible();
+    await expect(page.locator('#checkout-confirm-order-load')).toContainText('10.00');
+    await expect(page.locator('#checkout-confirm-order-load')).toContainText('17.00');
+    await page.getByRole('button', { name: 'Confirm' }).click();
+    await expect(page.locator('h1')).toContainText('Thank you');
+    await page.getByText('Your order has been successfully processed! Order number: 1824207 Click here').click();
+    await expect(page.locator('body')).toContainText('Your order has been successfully processed!');
+    await expect(page.locator('body')).toContainText('Order number: 1824207');
+    await expect(page.locator('body')).toContainText('Click here for order details.');
+    await page.getByText('Your order has been successfully processed! Order number: 1824207 Click here').click();
+    await page.getByRole('button', { name: 'Continue' }).click();
+
+    await loginPage.welcomeMessage.isVisible();
 });
